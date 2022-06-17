e97eb15eedd355e6271b0adc101e8a1dd64fa559
dmeadows <meadows.david@siemens.com>
2022-06-17 08:47:01 -0400

    commit project

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "blogappcoursedlm": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "blogappcoursedlm367817b7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
